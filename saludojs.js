5ab72b9fc04ac8f79d793608c34c16b575aee089
Afolox <afolox@gmail.com>
2016-05-21 13:37:14 -0600

    Added Saludos.js

---
+++ saludojs.js
@@ -0,0 +1,23 @@
+// JavaScript Document
+
+var saludoDIV = document.getElementById("saludo");
+
+console.log(saludoDIV);
+
+var GeneroP = window.prompt("Es usted hombre o mujer");
+
+console.log(GeneroP);
+
+var saludoHombre = "Tons que Mop?";
+var saludoMujeres = "Diay que Zorra?";
+
+	if(GeneroP =="hombre"){
+	saludoDIV.innerHTML = saludoHombre;
+	}
+	else if(GeneroP =="mujer"){
+	saludoDIV.innerHTML = saludoMujeres;
+	}
+	else{(GeneroP =="*")
+	saludoDIV.innerHTML = "ERROR";
+	}
+	
